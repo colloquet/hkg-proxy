adb1dc82442742511b6c1a1d730fc2a23e67a66f
Colloque Tsui <colloquet@gmail.com>
2018-05-03 22:12:39 +0800

    initial commit

---
+++ index.js
@@ -0,0 +1,34 @@
+const express = require('express')
+const cors = require('cors')
+const rp = require('request-promise')
+
+const app = express()
+
+app.use(cors({
+  origin: '*',
+  optionsSuccessStatus: 200,
+}))
+
+app.get('/', (req, res) => res.redirect('https://hkgolden.com/'))
+
+// https://web.hkgolden.com/api/topics/BW/1
+app.get('/api/topics/:cat/:page', async (req, res) => {
+  const { cat = 'BW', page = 1 } = req.params
+  const json = await rp({
+    uri: `https://web.hkgolden.com/api/topics/${cat}/${page}`,
+    json: true,
+  })
+  res.json(json)
+})
+
+// https://web.hkgolden.com/api/view/6897687/1
+app.get('/api/view/:id/:page', async (req, res) => {
+  const { id = 1, page = 1 } = req.params
+  const json = await rp({
+    uri: `https://web.hkgolden.com/api/view/${id}/${page}`,
+    json: true,
+  })
+  res.json(json)
+})
+
+app.listen(3000, () => console.log('Example app listening on port 3000!'))
